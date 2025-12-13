8b708afeab9de883e799eff652d074c3ca57eca4
NGM <ppmm91453@gmail.com>
2025-12-13 06:21:02 +0200

    add useScrollPosition

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ import TextRoll from './ui/text/TextRoll'
 
 export default function Hero() {
   return (
-    <div className="relative w-screen min-h-screen h- [300vh] flex flex-col justify-center items-center gap-12">
+    <div className="relative w-screen min-h-screen h-[300vh] flex flex-col justify-center items-center gap-12">
       <div className="absolute bottom-0 left-0 right-0 top-0 bg-[radial-gradient(125%_125%_at_50%_10%,rgba(255,255,255,0)_40%,rgba(102,51,238,1)_100%)]" />
       <TextAnimation text={`Urbnlanes Developments Building Masterpieces`} classname="text-6xl text-center" />
       <MainBtn to="not-found">
--- src/components/nav-components/Navbar.jsx
+++ src/components/nav-components/Navbar.jsx
@@ -1,5 +1,19 @@
-import React from 'react'
+'use client'
+
+import { useScrollPosition } from '@/hooks/useScrollPosition'
 
 export default function Navbar() {
-  return <div>Navbar</div>
+  const isScrolled20vh = useScrollPosition(0.2)
+  const isScrolled100vh = useScrollPosition(1)
+
+  return (
+    <header
+      className={`fixed w-screen min-h-34 border-b border-text/25 transition-all duration-700 z-50 
+        ${isScrolled20vh ? 'bg-bg' : ''}
+        ${isScrolled100vh ? '-top-full' : 'top-0'}
+      `}
+    >
+      <div>nav</div>
+    </header>
+  )
 }
