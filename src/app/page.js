d00f54ccee7c6cb0b026c3b07d71dad321396ec6
NGM <ppmm91453@gmail.com>
2026-01-08 22:38:39 +0200

    stay here

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { Suspense } from 'react'
 import { metadataGenerators } from '@/lib/seo-helpers'
 import LoadingSkeleton from '@/components/loading-components/LoadingSkeleton'
+import LoadingScreen from '@/components/loading-components/loadingScreen'
 import ProgressCarousel from '@/components/ProgressCarousel'
 import WhoWeAre from '@/components/WhoWeAre'
 import OurValues from '@/components/OurValues'
@@ -11,6 +12,7 @@ export const metadata = metadataGenerators.home()
 export default function Home() {
   return (
     <Suspense fallback={<LoadingSkeleton />}>
+      <LoadingScreen />
       <ProgressCarousel />
       <WhoWeAre />
       <OurValues />
