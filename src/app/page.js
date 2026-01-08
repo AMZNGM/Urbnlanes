87cabf8823eace3cd4cd5cb0ed92fef748069c52
NGM <ppmm91453@gmail.com>
2026-01-08 03:14:18 +0200

    home page done

--- src/app/page.js
+++ src/app/page.js
@@ -10,15 +10,11 @@ export const metadata = metadataGenerators.home()
 
 export default function Home() {
   return (
-    <>
-      <Suspense fallback={<LoadingSkeleton />}>
-        <ProgressCarousel />
-        <WhoWeAre />
-        <OurValues />
-        <SelectedProjects />
-
-        {/* <div className="top-0 right-0 bottom-0 left-0 absolute bg-[radial-gradient(125%_125%_at_50%_10%,rgba(255,255,255,0)_40%,rgba(102,51,238,1)_100%)]" />{' '} */}
-      </Suspense>
-    </>
+    <Suspense fallback={<LoadingSkeleton />}>
+      <ProgressCarousel />
+      <WhoWeAre />
+      <OurValues />
+      <SelectedProjects />
+    </Suspense>
   )
 }
--- src/components/loading-components/LoadingSkeleton.jsx
+++ src/components/loading-components/LoadingSkeleton.jsx
@@ -1,6 +1,6 @@
 export default function LoadingSkeleton() {
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="z-50 fixed inset-0 w-full h-full flex justify-center items-center">
       <div className="flex flex-col justify-center items-center gap-2 animate-pulse">
         <div className="w-32 h-8 bg-text/50 rounded-lg"></div>
         <div className="w-64 h-4 bg-text/40 rounded-lg"></div>
