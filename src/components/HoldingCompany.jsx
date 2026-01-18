22c7d01859e78c52e8bdf394e758b2d22c791c4f
NGM <ppmm91453@gmail.com>
2026-01-18 16:44:24 +0200

    fix HoldingCompany

--- src/components/HoldingCompany.jsx
+++ src/components/HoldingCompany.jsx
@@ -8,28 +8,30 @@ export default function HoldingCompany() {
   const { t } = useTranslation()
 
   return (
-    <section className="relative w-dvw overflow-hidden flex max-md:flex-col justify-center items-center rtl:gap-12 bg-text text-black">
-      <h6 as="h6" className="md:hidden space-x-4 font-bold text-[9dvw] text-center text-nowrap">
-        <span>{t('common.holding')}</span>
-        <span>{t('common.company')}</span>
-      </h6>
+    <section className="relative w-dvw overflow-hidden rtl:gap-12 bg-text text-black">
+      <div className="max-w-[90dvw] flex max-md:flex-col justify-center items-center mx-auto">
+        <h6 as="h6" className="md:hidden space-x-4 font-bold text-[8.5dvw] text-center text-nowrap">
+          <span>{t('common.holding')}</span>
+          <span>{t('common.company')}</span>
+        </h6>
 
-      <BreathingText as="h6" className="max-md:hidden font-bold text-[9dvw] text-center text-nowrap">
-        {t('common.holding')}
-      </BreathingText>
+        <BreathingText staggerFrom="first" className="max-md:hidden text-[8.2dvw] text-center text-nowrap">
+          {t('common.holding')}
+        </BreathingText>
 
-      <Image
-        src="/images/logos/eagroup-logo.webp"
-        alt="EA Group Logo"
-        width={400}
-        height={400}
-        sizes="10dvw"
-        className="z-10 relative ltr:w-full max-md:w-1/2 h-auto object-contain rtl:order-3 invert ltr:md:scale-125"
-      />
+        <Image
+          src="/images/logos/eagroup-logo.webp"
+          alt="EA Group Logo"
+          width={400}
+          height={400}
+          sizes="10dvw"
+          className="w-1/2 h-auto object-contain rtl:order-3 invert ltr:md:scale-125"
+        />
 
-      <BreathingText as="h6" staggerFrom="last" className="max-md:hidden font-bold text-[9dvw] text-center text-nowrap">
-        {t('common.company')}
-      </BreathingText>
+        <BreathingText staggerFrom="last" className="max-md:hidden text-[8.2dvw] text-center text-nowrap">
+          {t('common.company')}
+        </BreathingText>
+      </div>
     </section>
   )
 }
